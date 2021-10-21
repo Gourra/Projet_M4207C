01884fc8f9d5c5b1d0ed1964934bd9a3d0b6e9db
Guillaume <82163402+Gourra@users.noreply.github.com>
2021-10-21 12:11:15 +0200

    add comments

--- Test.java
+++ Test.java
@@ -1,12 +1,14 @@
 class Test{
 
     public static void main(String[] args){
+        // initialisation des objets
         Central C1 = new Central(1);
         Terminal T1 = new Terminal(1);
-        Data D1 = new Data("21/10/2021", 1, 12, "Degrés");
-        C1.addToBdd(D1);
-        T1.getBddData(C1);
-        T1.print(); 
+        Data D1 = new Data("21/10/2021", 1, 12, "Degrés"); //Dans le futur creer dans capteurs 
+        
+        C1.addToBdd(D1); //Ajout de la donnée D1 a la bdd du central
+        T1.getBddData(C1); //Transfers de la bbd central vers bddlocal du terminal
+        T1.print(); //Affichage de la bddlocale
     }
 
 }
