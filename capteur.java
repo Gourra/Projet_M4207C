cd9e615e08e7f156246cca303f17eaf81e7ebe78
ZedTheoT <86628399+ZedTheoT@users.noreply.github.com>
2021-10-21 12:03:17 +0200

    Update capteur.java

--- capteur.java
+++ capteur.java
@@ -1,16 +1,25 @@
-import java.text.DateFormat;
-import java.util.Date;
+import java.util.*;
+import java.text.DateFormat; 
 
-public class capteur {
-
-    /**
-     * //param args
-     */
-    public static void main(String[] args) {
+class capteur {
+    String liste [];
+    ArrayList<Object> val = new ArrayList<Object>();
+    public void chiant(){
+        
         Date aujourdhui = new Date();
-
         DateFormat shortDateFormat = DateFormat.getDateTimeInstance(DateFormat.SHORT, DateFormat.SHORT);
+        int n;
+        String v = "";
+        n = (int)(Math.random() * 50);
+        v = v + n;
 
-        System.out.println(shortDateFormat.format(aujourdhui));
+        String liste = String.format("%-20s %-20s %-5s %-20s", shortDateFormat.format(aujourdhui), "Capteur n°1", v, "Degré(s)");
+        val.add(liste);
+        //for (int i = 0; i < val.size(); i++) {
+        for (Object str:val){
+                
+            System.out.println(str); // Cette ligne permet d'afficher la Arraylist sans les crochets.
+        }
+        //}
     }
 }
