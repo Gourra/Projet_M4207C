7844b9839aabbb841e474c62e1c3af7acea66119
Guillaume <82163402+Gourra@users.noreply.github.com>
2021-10-21 10:27:59 +0200

    Add files via upload

---
+++ Test.java
@@ -0,0 +1,9 @@
+class Test{
+
+    public static void main(String[] args){
+        //Central C1 = new Central(1);
+        //String centralID = String.valueOf(C1.getId());
+        System.out.println("centralID");
+    }
+
+}
