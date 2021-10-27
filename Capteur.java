a5f97bcd65f460d2a8c4fceac398b57161658b82
Guillaume <82163402+Gourra@users.noreply.github.com>
2021-10-27 16:01:20 +0200

    comments

--- Capteur.java
+++ Capteur.java
@@ -23,19 +23,19 @@ class Capteur {
     //          METHODES
     //###################################
 
-    public String date(){
+    public String date(){ //genere la date d'une jolie facon
         Date date = new Date();
         DateFormat shortDateFormat = DateFormat.getDateTimeInstance(DateFormat.SHORT, DateFormat.SHORT);
         String liste = shortDateFormat.format(date);
         return liste;
     }
 
-    public int random(){
-        int n = (int)(Math.random() * 50);
+    public int random(){ //genere un nombre aleatoire entre 0 et 38
+        int n = (int)(Math.random() * 38);
         return n;
     }
 
-    public Data createData(){
+    public Data createData(){ //genere un objet data
         String date = date();
         int valeur = random();
 
